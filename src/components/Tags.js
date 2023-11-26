b8a708c3e3bca5c2b95eb0ad3b8575fda909f0af
WizardLM_AI <152059175+WizardLM@users.noreply.github.com>
2023-11-26 20:25:20 +0800

    Update Tags.js

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -4,14 +4,14 @@ import { useRouter } from "next/router";
 export default function Tags(props) {
   const router = useRouter();
 
-  const title = props.title ? props.title + " | LMSYS Org" : "LMSYS Org";
+  const title = props.title ? props.title + " | WizardLM" : "WizardLM";
   const desc = props.desc
     ? props.desc
-    : "LMSYS Org, Large Model Systems Organization, is an organization missioned to democratize the technologies underlying large models and their system infrastructures.";
+    : "WizardLM, Large Language Model.";
   const image = props.image ? props.image : "/social.png";
   const alt = props.alt
     ? props.alt
-    : "The text: LLMSYS Org, Large Model Systems Organization.";
+    : "The text: WizardLM, Large Language Model.";
   const slug = props.slug ? props.slug : router.route;
 
   return (
@@ -26,14 +26,14 @@ export default function Tags(props) {
       <meta property="og:description" content={desc} />
       <meta name="twitter:description" content={desc} />
 
-      <meta property="og:image" content={"https://lmsys.org" + image} />
-      <meta name="twitter:image" content={"https://lmsys.org" + image} />
+      <meta property="og:image" content={"https://wizardlm.github.io" + image} />
+      <meta name="twitter:image" content={"https://wizardlm.github.io" + image} />
 
       <meta name="twitter:image:alt" content={alt} />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={"https://lmsys.org" + slug} />
-      <meta name="twitter:url" content={"https://lmsys.org" + slug} />
+      <meta property="og:url" content={"https://wizardlm.github.io" + slug} />
+      <meta name="twitter:url" content={"https://wizardlm.github.io" + slug} />
       <meta name="twitter:card" content="summary_large_image" />
 
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
